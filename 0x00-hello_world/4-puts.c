c71fdc7ad469f76bed926cf5bbd3b230c6930472
yadsashel <ysalhi600@gmail.com>
2023-06-16 03:59:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - 4-puts.c
- * desription - Write a C program that prints exactly 
- *
- * Return: Always 0 (Success)
- */
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
- return (0);
-} 
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
