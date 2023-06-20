7b6bf2b03886ed431e93f526e1772052790924f2
yadsashel <ysalhi600@gmail.com>
2023-06-20 03:23:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - prints the string "0-putchar.c" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "0-putchar.c";
+	char c[] = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
