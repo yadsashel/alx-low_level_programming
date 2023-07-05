da3974351692897f86810568e0c24586393c5448
yadsashel <ysalhi600@gmail.com>
2023-07-05 04:48:13 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,18 +1,29 @@
-#include"main.h"
-#include <stdio.h>
-
-int wildcmp(char *, char *);
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int wildcmp(char *s1, char *s2)
 {
-	int r;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-	r = wildcmp("school.c", "*.c");
-	printf("%d\n", r);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
 	return (0);
 }
