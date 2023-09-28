3a0b6eaead8e7f68944a916fb00a771f7a856a1d
yadsashel <ysalhi600@gmail.com>
2023-09-28 05:05:46 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
-
 /**
- * _puts_recursion - Prints a string followed by a new line.
+ * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') // Check if we have reached the end of the string (null terminator).
-    {
-        putchar('\n'); // Print a newline character to move to the next line.
-        return; // Exit the function.
-    }
-
-    putchar(*s); // Print the current character.
-    _puts_recursion(s + 1); // Recursively call the function with the next character.
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
 
