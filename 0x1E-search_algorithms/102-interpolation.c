6d177bc48ea775a22d383f050681994d00ac35cf
yadsashel <ysalhi600@gmail.com>
2024-07-17 03:06:33 -0700

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,43 +1,52 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - function that searches for a value in
- * an array of integers using the interpolation search algorithm
- * @array: array of integers
- * @size: size of array
- * @value: value that is searched in array
- * Return: return the index of value, and if it doesn't exist return -1
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low, mid, high;
+	size_t pos, low, high;
+	double f;
 
-	low = 0;
-	high = (int) size - 1;
-
-	if (!array || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	while (array[low] != array[high] && value >= array[low] &&
-	       value <= array[high])
+	low = 0;
+	high = size - 1;
+
+	while (size)
 	{
-		mid = low + (((double) (high - low) / (array[high] - array[low])) *
-		             (value - array[low]));
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
 
-		printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
-
-		if (value > array[mid])
-			low = mid + 1;
-		else if (value < array[mid])
-			high = mid - 1;
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
 		else
-			return (mid);
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
 	}
-
-	mid = low + (((double) (high - low) / (array[high] - array[low])) *
-	             (value - array[low]));
-
-	printf("Value checked array[%ld] is out of range\n", mid);
 
 	return (-1);
 }
